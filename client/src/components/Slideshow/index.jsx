0dde227fb340d4d6a0d40a0e2ce2f588154de4e8
KevinRhode <77312186+KevinRhode@users.noreply.github.com>
2023-09-22 16:33:16 -0400

    add: boarder issue fixed. working on slide orientation

--- client/src/components/Slideshow/index.jsx
+++ client/src/components/Slideshow/index.jsx
@@ -49,7 +49,7 @@ function Slideshow() {
 
           >
            {/* {console.log(background)} */}
-           <p>Testing</p>
+           {/* <p>Testing</p> */}
             {/* <img src={background}  style={{ width: "100%", height: "auto"}} /> */}
           </div>
         ))}
